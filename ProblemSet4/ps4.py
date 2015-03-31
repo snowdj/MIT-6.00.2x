667889aa00e08ee07492269ccbbd61077d03f472
Mark R. Lytell <lytemar@yahoo.com>
2015-03-31 17:48:38 -0500

    First attempt at problem set 4

--- ProblemSet4/ps4.py
+++ ProblemSet4/ps4.py
@@ -5,6 +5,17 @@ import random
 import pylab
 from ps3b import *
 
+
+# Helper functions
+
+def createViruses(numViruses, maxBirthProb, clearProb, resistances, mutProb):
+    viruses = []
+    for j in range(numViruses):
+            viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+    return viruses
+    
+import math
+
 #
 # PROBLEM 1
 #        
@@ -22,8 +33,64 @@ def simulationDelayedTreatment(numTrials):
     numTrials: number of simulation runs to execute (an integer)
     """
     
-    # TODO
+    # Simulation parameters
+    maxPop = 1000
+    numViruses = 100
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol': False}
+    mutProb = 0.005
+    
+    StepsBeforeDrug = [300, 150, 75, 0]
+    StepsWithDrug = 150   
+    timesteps = [x + StepsWithDrug for x in StepsBeforeDrug]
+    virusesList = []
+    guttagonolResVirusesList = []
+    
+    
+    for i in range(len(StepsBeforeDrug)):
+        # Initialize the running total and average lists to all zeros
+        virusesAtTime = []
+        guttagonolResistantViruses = []
+        virusesAtTime = [0] * timesteps[i]
+        guttagonolResistantViruses = [0] * timesteps[i]
+  
+        for j in range(numTrials):
+        
+            viruses = []
 
+            # Create ResistantVirus intances
+            viruses = createViruses(numViruses, maxBirthProb,
+                clearProb, resistances, mutProb)
+        
+            # Create the patient
+            patient = TreatedPatient(viruses, maxPop)
+        
+            # run the timesteps
+            for t in range(timesteps[i]):
+                if t == StepsBeforeDrug[i]:
+                    patient.addPrescription("guttagonol")
+                virusesAtTime[t] += patient.update()
+                guttagonolResistantViruses[t] += patient.getResistPop(['guttagonol'])
+         
+        virusesList.append(virusesAtTime[:])
+        guttagonolResVirusesList.append(guttagonolResistantViruses[:])
+            
+        numBins = math.ceil( numTrials**0.5 )
+            
+        pylab.subplot(len(StepsBeforeDrug), 2, 2*i+1)
+        pylab.hist(virusesList[i], bins = numBins)
+        pylab.xlabel('Total Virus Population')
+        pylab.ylabel('Number of Trials')
+            
+        pylab.subplot(len(StepsBeforeDrug), 2, 2*i+2)
+        pylab.hist(guttagonolResVirusesList[i], bins = numBins)
+        pylab.xlabel('Guttagonol Resistant Virus Population')
+        pylab.ylabel('Number of Trials')
+ 
+    pylab.show()    
+    
+    return None
 
 
 
