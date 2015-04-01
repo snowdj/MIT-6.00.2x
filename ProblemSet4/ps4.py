f7022a23297b9f56659a64725f263b6492d5c911
lytemar@yahoo.com <lytemar@yahoo.com>
2015-03-31 21:02:00 -0500

    Completed problem set 4

--- ProblemSet4/ps4.py
+++ ProblemSet4/ps4.py
@@ -8,13 +8,8 @@ from ps3b import *
 
 # Helper functions
 
-def createViruses(numViruses, maxBirthProb, clearProb, resistances, mutProb):
-    viruses = []
-    for j in range(numViruses):
-            viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
-    return viruses
-    
 import math
+random.seed(12345)
 
 #
 # PROBLEM 1
@@ -45,23 +40,20 @@ def simulationDelayedTreatment(numTrials):
     StepsWithDrug = 150   
     timesteps = [x + StepsWithDrug for x in StepsBeforeDrug]
     virusesList = []
-    guttagonolResVirusesList = []
     
     
     for i in range(len(StepsBeforeDrug)):
-        # Initialize the running total and average lists to all zeros
-        virusesAtTime = []
-        guttagonolResistantViruses = []
-        virusesAtTime = [0] * timesteps[i]
-        guttagonolResistantViruses = [0] * timesteps[i]
+        virusesAtTrial = [0] * numTrials
   
         for j in range(numTrials):
+            
+            virusesAtTime = [0] * timesteps[i]
         
             viruses = []
 
             # Create ResistantVirus intances
-            viruses = createViruses(numViruses, maxBirthProb,
-                clearProb, resistances, mutProb)
+            for k in range(numViruses):
+                viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
         
             # Create the patient
             patient = TreatedPatient(viruses, maxPop)
@@ -70,31 +62,23 @@ def simulationDelayedTreatment(numTrials):
             for t in range(timesteps[i]):
                 if t == StepsBeforeDrug[i]:
                     patient.addPrescription("guttagonol")
-                virusesAtTime[t] += patient.update()
-                guttagonolResistantViruses[t] += patient.getResistPop(['guttagonol'])
-         
-        virusesList.append(virusesAtTime[:])
-        guttagonolResVirusesList.append(guttagonolResistantViruses[:])
+                virusesAtTime[t] = patient.update()
+            
+            virusesAtTrial[j] = virusesAtTime[-1]
             
         numBins = math.ceil( numTrials**0.5 )
+        
+        percentCured = sum(x<=50 for x in virusesAtTrial)/float(sum(x>=0 for x in virusesAtTrial))*100
+        print('Percent cured for delay = ' + str(StepsBeforeDrug[i]) + ': %.2f' % percentCured)
             
-        pylab.subplot(len(StepsBeforeDrug), 2, 2*i+1)
-        pylab.hist(virusesList[i], bins = numBins)
-        pylab.xlabel('Total Virus Population')
-        pylab.ylabel('Number of Trials')
-            
-        pylab.subplot(len(StepsBeforeDrug), 2, 2*i+2)
-        pylab.hist(guttagonolResVirusesList[i], bins = numBins)
-        pylab.xlabel('Guttagonol Resistant Virus Population')
+        pylab.subplot(len(StepsBeforeDrug)/2, 2, i+1)
+        pylab.hist(virusesAtTrial, bins = numBins)
+        pylab.xlabel('Total Virus Population, Delay = ' + str(StepsBeforeDrug[i]))
         pylab.ylabel('Number of Trials')
  
     pylab.show()    
     
     return None
-
-
-
-
 
 #
 # PROBLEM 2
@@ -112,4 +96,60 @@ def simulationTwoDrugsDelayedTreatment(numTrials):
 
     numTrials: number of simulation runs to execute (an integer)
     """
-    # TODO
+    
+    # Simulation parameters
+    maxPop = 1000
+    numViruses = 100
+    maxBirthProb = 0.1
+    clearProb = 0.05
+    resistances = {'guttagonol': False, 'grimpex': False}
+    mutProb = 0.005
+    
+    StepsBetweenDrugs = [300, 150, 75, 0]
+    InitialStepsWithoutDrug = 150
+    StepsWithSecondDrug = 150   
+    timesteps = [x + StepsWithSecondDrug + InitialStepsWithoutDrug for x in StepsBetweenDrugs]
+    
+    for i in range(len(StepsBetweenDrugs)):
+        virusesAtTrial = [0] * numTrials
+  
+        for j in range(numTrials):
+            
+            virusesAtTime = [0] * timesteps[i]
+        
+            viruses = []
+
+            # Create ResistantVirus intances
+            for k in range(numViruses):
+                viruses.append(ResistantVirus(maxBirthProb, clearProb, resistances, mutProb))
+        
+            # Create the patient
+            patient = TreatedPatient(viruses, maxPop)
+        
+            # run the timesteps
+            for t in range(timesteps[i]):
+                if t == InitialStepsWithoutDrug:
+                    patient.addPrescription('guttagonol')
+                if t == InitialStepsWithoutDrug + StepsBetweenDrugs[i]:
+                    patient.addPrescription('grimpex')
+                virusesAtTime[t] = patient.update()
+            
+            virusesAtTrial[j] = virusesAtTime[-1]
+            
+        numBins = math.ceil( numTrials**0.5 )
+        
+        percentCured = sum(x<=50 for x in virusesAtTrial)/float(sum(x>=0 for x in virusesAtTrial))*100
+        varianceViruses = numpy.var(virusesAtTrial)
+        print('Percent cured for time between drugs = ' + \
+            str(StepsBetweenDrugs[i]) + ': %.2f' % percentCured)
+        print('Simulation variance for time between drugs = ' + \
+            str(StepsBetweenDrugs[i]) + ': %.2f' % varianceViruses)
+            
+        pylab.subplot(len(StepsBetweenDrugs)/2, 2, i+1)
+        pylab.hist(virusesAtTrial, bins = numBins)
+        pylab.xlabel('Total Virus Population, Time Between Drugs = ' + str(StepsBetweenDrugs[i]))
+        pylab.ylabel('Number of Trials')
+ 
+    pylab.show()    
+    
+    return None
